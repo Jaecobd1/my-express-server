4795e5e8486c33d77c355387b827bd85737979bd
Jaecobd1 <jaecob.d1@gmail.com>
2022-06-10 15:20:24 -0400

    update server.js

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ const Port = 3000;
 
 app.get("/", function (req, res) {
     res.send("Hello");
-})
+});
+
+app.get("/contact", function (req, res) {
+    res.send("Contact me at: jake@doblermedia.com");
+});
+
+app.get("/about", function (req, res) {
+    res.send("Hi I'm Jake");
+});
+
+app.get("/hobbies", function (req, res) {
+    res.send("coding");
+});
 
 app.listen(Port, function () {
     console.log(`Server started on port ${Port}`);
